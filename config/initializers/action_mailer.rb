8cb1157561b52858ab77e52ade42277e26a0e57c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-12-09 13:10:52 -0800

    Use different settings in the different areas

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,9 +1,20 @@
-Rails.application.config.action_mailer.default_url_options = {
-  host: ENVied.RAILS_HOST,
-  port: ENV["PORT"]
-}
-
 ActionMailer::Base.delivery_method = :smtp
+
+case
+when ENV["HEROKU_APP_NAME"]
+  Rails.application.config.action_mailer.default_url_options = {
+    host: "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com"
+  }
+when Rails.env.production?
+  Rails.application.config.action_mailer.default_url_options = {
+    host: ENVied.RAILS_HOST
+  }
+else
+  Rails.application.config.action_mailer.default_url_options = {
+    host: ENVied.RAILS_HOST,
+    port: ENV["PORT"]
+  }
+end
 
 case
 when ENV["MAILTRAP_API_TOKEN"]
@@ -12,17 +23,18 @@ when ENV["MAILTRAP_API_TOKEN"]
       user_name: inbox["username"],
       password: inbox["password"],
       address: inbox["domain"],
-      domain: inbox["domain"],
+      domain: ENVied.RAILS_HOST,
       port: inbox["smtp_ports"].first,
       authentication: :plain
     }
   end
 when ENV["MAILGUN_SMTP_PORT"]
+
   ActionMailer::Base.smtp_settings = {
     user_name: ENV["MAILGUN_SMTP_LOGIN"],
     password: ENV["MAILGUN_SMTP_PASSWORD"],
     address: ENV["MAILGUN_SMTP_SERVER"],
-    domain: ENV["RAILS_HOST"],
+    domain: ENVied.RAILS_HOST,
     port: ENV["MAILGUN_SMTP_PORT"],
     authentication: :plain,
   }
