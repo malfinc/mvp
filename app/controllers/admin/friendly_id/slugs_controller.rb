8e2b8c7bfd89b822add5d2f55a542277a5cbc2fa
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-12-30 23:28:27 -0800

    Now we can see slugs in the admin dashboard

---
+++ app/controllers/admin/friendly_id/slugs_controller.rb
@@ -0,0 +1,21 @@
+module Admin
+  module FriendlyId
+    class SlugsController < Admin::ApplicationController
+      # To customize the behavior of this controller,
+      # you can overwrite any of the RESTful actions. For example:
+      #
+      def index
+        super
+        @resources = ::FriendlyId::Slug.page(params[:page]).per(10)
+      end
+
+      # Define a custom finder by overriding the `find_resource` method:
+      private def find_resource(slug)
+        ::FriendlyId::Slug.find_by(slug: slug)
+      end
+
+      # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
+      # for more information
+    end
+  end
+end
