37ee1065d8894a92a5c510c873bd31edb160a6c2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-01-01 11:52:47 -0800

    We need to be able to set the current account on the model

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -10,9 +10,34 @@ module Admin
     #     per(10)
     # end
 
+    def new
+      render locals: {
+        page: Administrate::Page::Form.new(dashboard, resource_class.new.tap { |record| record.current_account = current_account }),
+      }
+    end
+
+    def create
+      resource = resource_class.new(resource_params).tap { |record| record.current_account = current_account }
+
+      if resource.save
+        redirect_to(
+          [namespace, resource],
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }
+      end
+    end
+
     # Define a custom finder by overriding the `find_resource` method:
     private def find_resource(identifier)
       Recipe.friendly.find(identifier)
+    end
+
+    private def permitted_attributes
+      super + [:state_event]
     end
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
