0ba658136e4ce14c23cd7f28d95d0aaaab36d7ca
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-01-02 10:15:45 -0800

    It's a gem, so isolating it

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,4 @@
-Devise::Async.backend = :sidekiq
+
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
