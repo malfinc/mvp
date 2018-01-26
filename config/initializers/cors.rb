13ee3e6ea73d0c1e6969b8c43b4bbe88e01fd692
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-01-26 10:09:32 -0800

    We don't use envied anymore

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENVied.WWW_LOCATION.gsub(/https?::\/\//, '')
+    origins ENV.fetch("WWW_LOCATION").gsub(/https?::\/\//, '')
 
     resource "*",
       headers: :any,
