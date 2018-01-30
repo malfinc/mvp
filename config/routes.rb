d0e4de82ab6aa90e9e1aa9bf8c9c9926607fcb88
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-01-30 01:02:01 -0800

    Setting up jsonapi routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :v1 do
+    jsonapi_resources :accounts
+    jsonapi_resources :carts
+    jsonapi_resources :cart_items
+    jsonapi_resources :billing_informations
+    jsonapi_resources :shipping_informations
+    jsonapi_resources :payments
+    jsonapi_resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
