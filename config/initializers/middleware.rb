49dc6c3d19b3c53d18cca0b86de9d56e8025e4d1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-02-03 16:49:30 -0800

    Cookies are fantastic

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,3 +1,7 @@
-BlankApiRails::Application.config.middleware.use(Rack::AuthenticationBearer)
-BlankApiRails::Application.config.middleware.use(Rack::Deflater)
-BlankApiRails::Application.config.middleware.use(Rack::Attack)
+BlankApiRails::Application.config.middleware.tap do |middleware|
+  middleware.use Rack::AuthenticationBearer
+  middleware.use Rack::Deflater
+  middleware.use Rack::Attack
+  middleware.use ActionDispatch::Cookies
+  middleware.use ActionDispatch::Session::CookieStore, key: '_blank-api-rails_key'
+end
