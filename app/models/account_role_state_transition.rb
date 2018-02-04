39c6598cc013a3f05b7b61e21a5a7e31b90370be
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-02-03 17:32:51 -0800

    The first transition never has a from, and apparently namespace is for something else

--- app/models/account_role_state_transition.rb
+++ app/models/account_role_state_transition.rb
@@ -1,7 +1,5 @@
 class AccountRoleStateTransition < ApplicationRecord
   belongs_to :account
 
-  validates_presence_of :namespace
-  validates_presence_of :from
   validates_presence_of :to
 end
--- app/models/recipe_queue_state_transition.rb
+++ app/models/recipe_queue_state_transition.rb
@@ -1,7 +1,5 @@
 class RecipeQueueStateTransition < ApplicationRecord
   belongs_to :recipe
 
-  validates_presence_of :namespace
-  validates_presence_of :from
   validates_presence_of :to
 end
