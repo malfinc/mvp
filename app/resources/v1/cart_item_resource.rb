6d6964033d5ac1ee7e48079d42a5dbcb144295d2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-02-06 08:23:54 -0800

    We need this controller data

--- app/resources/v1/cart_item_resource.rb
+++ app/resources/v1/cart_item_resource.rb
@@ -1,5 +1,8 @@
 module V1
   class CartItemResource < ::V1::ApplicationResource
+    record_context :current_account
+    record_context :current_cart
+
     has_one :account
     has_one :cart
     has_one :product
