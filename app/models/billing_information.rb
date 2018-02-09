885270a8f9b512ba490804cd210110c9b04f1cdc
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-02-09 00:37:42 -0800

    We want to associate shopping/billing to accounts

--- app/models/billing_information.rb
+++ app/models/billing_information.rb
@@ -1,9 +1,24 @@
 class BillingInformation < ApplicationRecord
   has_many :carts
+  belongs_to :account
 
   validates_presence_of :name
   validates_presence_of :address
   validates_presence_of :postal
   validates_presence_of :city
   validates_presence_of :state
+
+  attr_accessor :current_account
+  attr_accessor :current_cart
+
+  before_validation :associate_current_cart
+  before_validation :associate_current_account
+
+  private def associate_current_cart
+    self.carts << current_cart.assign_attributes(billing_information: self)
+  end
+
+  private def associate_current_account
+    assign_attributes(account: current_account)
+  end
 end
--- app/resources/v1/billing_information_resource.rb
+++ app/resources/v1/billing_information_resource.rb
@@ -1,5 +1,8 @@
 module V1
   class BillingInformationResource < ::V1::ApplicationResource
+    record_context :current_account
+    record_context :current_cart
+
     has_one :account
     has_one :cart
   end
