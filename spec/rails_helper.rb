a4b890a15fba81b57a0beca5e2f42a329068902d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-02-12 00:26:02 -0800

    More of a rails thing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,10 @@ require "rspec/rails"
 #
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+require_relative "support/matchers/jsonapi_response"
+require_relative "support/contexts/jsonapi_request"
+require_relative "support/contexts/jsonapi_requests"
+
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
