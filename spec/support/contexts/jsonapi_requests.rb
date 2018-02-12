299ee29b8a381ce74196729210fb93af26d79243
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-02-12 00:25:31 -0800

    Making multi-request version of our request contexts

---
+++ spec/support/contexts/jsonapi_requests.rb
@@ -0,0 +1,49 @@
+RSpec.shared_context "JSON:API requests" do
+  def default_headers(content: nil, authentication: nil)
+    {
+      "Accept" => "application/vnd.api+json",
+      "Content-Type" => if content.present? then "application/vnd.api+json" end,
+      "Authentication" => if authentication.present? then "Bearer #{Base64.urlsafe_encode64(authentication)}" end
+    }.compact
+  end
+
+  def jsonapi_update(path:, id:, type:, authentication: nil, custom_headers: {}, **data)
+    put(
+      path,
+      headers: default_headers(content: true, authentication: authentication).merge(custom_headers || {}),
+      params: payload(id: id, type: type, **data)
+    )
+  end
+
+  def jsonapi_create(path:, type:, authentication: nil, custom_headers: {}, **data)
+    post(
+      path,
+      headers: default_headers(content: true, authentication: authentication).merge(custom_headers || {}),
+      params: payload(type: type, **data)
+    )
+  end
+
+  def payload(id: nil, type:, attributes: nil, relationships: nil, metadata: nil, included: nil)
+    Oj.dump(
+      {
+        data: {
+          id: if id.present? then id end,
+          type: type,
+          attributes: if attributes.present? then attributes.compact end,
+          relationships: if relationships.present? then relationships.compact end,
+        }.compact,
+        metadata: if metadata.present? then metadata.compact end,
+        included: if included.present? then included.compact end,
+      }.compact.deep_stringify_keys
+    )
+  end
+
+  def relationship(id:, type:)
+    {
+      data: {
+        id: id,
+        type: type
+      }
+    }
+  end
+end
