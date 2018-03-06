a5430a1b3776ecca32b6586259cc9d81516dd3e0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-03-05 16:06:46 -0800

    Giving the accounts more states

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ApplicationRecord
   include FriendlyId
+  include AuditActor
 
   USERNAME_PATTERN = /\A[a-zA-Z0-9_-]+\z/i
 
@@ -18,40 +19,48 @@ class Account < ApplicationRecord
   devise :recoverable
   devise :registerable
   devise :rememberable
-  devise :timeoutable
   devise :validatable
   devise :async
 
+  before_validation :generate_password, unless: :encrypted_password?
+  before_validation :generate_authentication_secret, unless: :authentication_secret?
+
+  validates_presence_of :username, if: :email_required?
+  validates_format_of :username, with: USERNAME_PATTERN, if: :email_required?
+
+  state_machine :onboarding_state, initial: :fresh do
+    audit_trail initial: false, context: :audit_actor_id
+
+    event :convert do
+      transition from: :fresh, to: :converted
+    end
+
+    event :complete do
+      transition from: :converted, to: :completed
+    end
+  end
+
   state_machine :role_state, initial: :user do
+    audit_trail initial: false, context: :audit_actor_id
+
     event :empower do
       transition user: :moderator
     end
 
-    state :user do
-      validates_presence_of :username
-      validates_format_of :username, with: USERNAME_PATTERN
-
-      def user?
-        true
-      end
+    event :spark do
+      transition user: :administrator
     end
+  end
 
-    state :moderator do
-      validates_presence_of :username
-      validates_format_of :username, with: USERNAME_PATTERN
+  private def generate_password
+    assign_attributes(password: SecureRandom.hex(60))
+  end
 
-      def moderator?
-        true
-      end
-    end
+  private def generate_authentication_secret
+    assign_attributes(authentication_secret: SecureRandom.hex(60))
+  end
 
-    state :administrator do
-      validates_presence_of :username
-      validates_format_of :username, with: USERNAME_PATTERN
-
-      def administrator?
-        true
-      end
-    end
+  private def email_required?
+    onboarding_state?(:converted) || onboarding_state?(:completed)
   end
 end
