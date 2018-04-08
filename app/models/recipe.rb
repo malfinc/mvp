8ccdc5fa394aa2c14b249592f5f363871eb717f8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-04-08 15:45:29 -0700

    We've moved this to a concern

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -16,39 +16,6 @@ class Recipe < ApplicationRecord
   validates_presence_of :updated_at, on: :update
 
   friendly_id :name, :use => [:slugged, :history]
-
-  state_machine :queue_state, initial: :draft do
-    audit_trail initial: false, context: :audit_actor_id
-
-    event :publish do
-      transition :draft => :queued
-    end
-
-    event :publish do
-      transition :draft => :published, if: :allowed_to_autopublish?
-    end
-
-    event :remove do
-      transition :published => :draft, if: :allowed_to_remove?
-    end
-
-    event :deny do
-      transition :queued => :draft, if: :allowed_to_deny?
-    end
-
-    event :approve do
-      transition :queued => :published, if: :allowed_to_approve?
-    end
-
-    state :removed do
-      validates_presence_of :remover
-    end
-
-    state :published do
-      validates_presence_of :publisher
-    end
-  end
-
   private def should_generate_new_friendly_id?
     super || send("#{friendly_id_config.base}_changed?")
   end
