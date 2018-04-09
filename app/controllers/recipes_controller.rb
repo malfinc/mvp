9f567d3a835e5a9f98473ffeacff012eaefd2b88
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-04-08 20:02:08 -0700

    Only create recipes for this user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     authenticate_account!
-    @record = Recipe.new
+    @record = current_account.recipes.new
   end
 
   # GET /recipes/1/edit
@@ -25,7 +25,7 @@ class RecipesController < ApplicationController
   # POST /recipes
   def create
     authenticate_account!
-    @record = Recipe.new(recipe_params)
+    @record = current_account.recipes.new(recipe_params)
     authorize_account!
 
     if @record.save
