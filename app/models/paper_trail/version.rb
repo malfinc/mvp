7598894d511b7f3d5d85780a02a5ee9ce2d82c2c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-04-08 21:36:02 -0700

    Only use super if it returns an active record object

--- app/models/paper_trail/version.rb
+++ app/models/paper_trail/version.rb
@@ -5,6 +5,10 @@ class PaperTrail::Version < ::ActiveRecord::Base
   belongs_to :item, polymorphic: true
 
   def actor
-    super || ActorNull.new(name: whodunnit, username: whodunnit)
+    if super.kind_of?(ActiveRecord::Base)
+      super
+    else
+      ActorNull.new(name: whodunnit, username: whodunnit)
+    end
   end
 end
