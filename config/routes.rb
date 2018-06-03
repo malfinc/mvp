8c79e66ad3d310063bdabb9bb058586315af9d1a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-06-02 18:29:10 -0700

    Limit what routes we allow

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   mount JSONAPI::Home::Engine, at: "/"
 
   namespace :v1 do
-    resources :accounts
-    resources :billing_informations, path: "billing-informations"
-    resources :delivery_informations, path: "delivery-informations"
-    resources :cart_items, path: "cart-items"
-    resources :carts
-    resources :products
-    resources :payments
+    resources :accounts, only: [:index ,:show ,:create ,:update]
+    resources :billing_informations, path: "billing-informations", only: [:index ,:show ,:create ,:update]
+    resources :delivery_informations, path: "delivery-informations", only: [:index ,:show ,:create ,:update]
+    resources :cart_items, path: "cart-items", only: [:index ,:show ,:create, :destroy]
+    resources :carts, only: [:show ,:update]
+    resources :products, only: [:index ,:show]
+    resources :payments, only: [:index ,:show ,:create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
