79b36857cf988a5c56282df3aea63963197a1ae6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-06-25 23:18:12 -0700

    Some more fine tuning

--- app/models/application_computed.rb
+++ app/models/application_computed.rb
@@ -9,7 +9,7 @@ class ApplicationComputed
     self.id ||= attributes[:id] || SecureRandom.uuid
     super(
       **attributes.merge(
-        :created_at => if created_at.nil? then attributes[:created_at] || Time.now end,
+        :created_at => if created_at.nil? then attributes[:created_at] || Time.zone.now end,
         :updated_at => Time.zone.now
       )
     )
