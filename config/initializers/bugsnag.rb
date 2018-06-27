bcbbd0879f740f95a17547a1142afe27bfbb676f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-06-27 16:03:36 -0700

    App version is effectively the checksum

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,13 +1,9 @@
-
-system "env"
-Rails.logger.info `env`
-
 if Rails.env.production?
   Bugsnag.configure do |configuration|
     configuration.auto_notify = Rails.env.production?
     configuration.auto_capture_sessions = Rails.env.production?
     configuration.api_key = ENV.fetch("BUGSNAG_API_KEY", nil)
-    configuration.app_version = ENV.fetch("HEROKU_RELEASE_VERSION")
+    configuration.app_version = ENV.fetch("SOURCE_VERSION")
     configuration.notify_release_stages = ["production"]
   end
 end
