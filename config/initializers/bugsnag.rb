e9440ae99ad35e34db31d66c7b04c1b6a1928045
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-06-27 09:58:02 -0700

    Release apps don't have bugsnag, because $

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   Bugsnag.configure do |configuration|
     configuration.auto_notify = Rails.env.production?
     configuration.auto_capture_sessions = Rails.env.production?
-    configuration.api_key = ENV.fetch("BUGSNAG_API_KEY")
+    configuration.api_key = ENV.fetch("BUGSNAG_API_KEY", nil)
     configuration.app_version = ENV.fetch("HEROKU_RELEASE_VERSION")
     configuration.notify_release_stages = ["production"]
   end
