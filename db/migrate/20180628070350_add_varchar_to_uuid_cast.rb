adbdc6dc305b87f72cd9bde02e03a37cd9dab27d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-07-01 20:58:49 -0700

    Attempt to fix for a permissions issue

--- db/migrate/20180628070350_add_varchar_to_uuid_cast.rb
+++
@@ -1,7 +0,0 @@
-class AddVarcharToUuidCast < ActiveRecord::Migration[5.1]
-  def change
-    safety_assured do
-      execute(%(CREATE CAST (varchar AS uuid) WITH INOUT AS IMPLICIT))
-    end
-  end
-end
