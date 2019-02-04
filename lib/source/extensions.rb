1f22bb816ea80397d599f0d0faffe60a4fd21999
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-02-03 16:20:04 -0800

    Whoops, wrong path

--- lib/source/extensions.rb
+++ lib/source/extensions.rb
@@ -1,3 +1,3 @@
-require_relative("source/extensions/action_dispatch_http_headers_inspect")
-require_relative("source/extensions/rails_console_account_access")
-require_relative("source/extensions/pundit_not_authorized_error_message")
+require_relative("extensions/action_dispatch_http_headers_inspect")
+require_relative("extensions/rails_console_account_access")
+require_relative("extensions/pundit_not_authorized_error_message")
