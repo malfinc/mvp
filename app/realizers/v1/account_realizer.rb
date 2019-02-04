8da0e730fff6fc376ec6e7ffec3e07bb01cc2bce
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-02-04 00:48:44 -0800

    We have to provide a class name

--- app/realizers/v1/account_realizer.rb
+++ app/realizers/v1/account_realizer.rb
@@ -2,7 +2,7 @@ module V1
   class AccountRealizer < ApplicationRealizer
     type(:accounts, :class_name => "Account", :adapter => :active_record)
 
-    has_many(:payments, as: :payments)
+    has_many(:payments, :as => :payments, :class_name => "PaymentRealizer")
 
     has(:email)
     has(:name)
