1500e0780b7e9b57c8fdb2cd6fb31205afc512b3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-02-03 16:19:59 -0800

    Much better names

--- lib/source/configuration.rb
+++ lib/source/configuration.rb
@@ -1,14 +1,14 @@
-  REDIS_SIDEKIQ_SERVER_CONNECTION_POOL = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_SIDEKIQ_SERVER_POOL_SIZE")), :timeout => 5) do
 module BlankApiRails
+  REDIS_SIDEKIQ_SERVER_CONNECTION = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_SIDEKIQ_SERVER_POOL_SIZE")), :timeout => 5) do
     ::Redis.new(:driver => :hiredis, :url => ENV.fetch("REDIS_SIDEKIQ_URL"))
   end
-  REDIS_SIDEKIQ_CLIENT_CONNECTION_POOL = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_SIDEKIQ_CLIENT_POOL_SIZE")), :timeout => 5) do
+  REDIS_SIDEKIQ_CLIENT_CONNECTION = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_SIDEKIQ_CLIENT_POOL_SIZE")), :timeout => 5) do
     ::Redis.new(:driver => :hiredis, :url => ENV.fetch("REDIS_SIDEKIQ_URL"))
   end
-  REDIS_OBJECTS_CONNECTION_POOL = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_OBJECTS_POOL_SIZE")), :timeout => 5) do
+  REDIS_OBJECTS_CONNECTION = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_OBJECTS_POOL_SIZE")), :timeout => 5) do
     ::Redis.new(:driver => :hiredis, :url => ENV.fetch("REDIS_OBJECTS_URL"))
   end
-  REDIS_CACHE_CONNECTION_POOL = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_CACHE_POOL_SIZE")), :timeout => 5) do
+  REDIS_CACHE_CONNECTION = ConnectionPool.new(:size => Integer(ENV.fetch("REDIS_CACHE_POOL_SIZE")), :timeout => 5) do
     ::Redis.new(:driver => :hiredis, :url => ENV.fetch("REDIS_CACHE_URL"))
   end
   REDIS_LOCK_CONNECTION = Redlock::Client.new([
