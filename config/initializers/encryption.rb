d64c6d2c94b57a56251d8e2523c48dd5ddc33ea5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-02-03 16:56:30 -0800

    Wrap in an if based on the file’s existence

--- config/initializers/encryption.rb
+++ config/initializers/encryption.rb
@@ -1,3 +1,5 @@
-ENCRYPTION_KEY_GENERATOR = ActiveSupport::KeyGenerator.new(Rails.application.credentials.key)
-ENCRYPTION_KEY = ENCRYPTION_KEY_GENERATOR.generate_key(Rails.application.credentials.fetch(:encryption).fetch(:salt), 32)
-ENCRYPTOR = ActiveSupport::MessageEncryptor.new(ENCRYPTION_KEY)
+if File.exists?(Rails.root.join("config", "credentials.yml.enc"))
+  ENCRYPTION_KEY_GENERATOR = ActiveSupport::KeyGenerator.new(Rails.application.credentials.key)
+  ENCRYPTION_KEY = ENCRYPTION_KEY_GENERATOR.generate_key(Rails.application.credentials.fetch(:encryption).fetch(:salt), 32)
+  ENCRYPTOR = ActiveSupport::MessageEncryptor.new(ENCRYPTION_KEY)
+end
