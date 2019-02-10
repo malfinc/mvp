2e5f156428f1283fb5f490186f74d2316777ed8f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-02-10 12:53:11 -0800

    We’re not going to be using these services in the future

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,9 +1,10 @@
-if Rails.env.production?
-  Bugsnag.configure do |configuration|
-    configuration.auto_notify = Rails.env.production?
-    configuration.auto_capture_sessions = Rails.env.production?
-    configuration.api_key = ENV.fetch("BUGSNAG_API_KEY", nil)
-    configuration.app_version = ENV.fetch("HEROKU_RELEASE_VERSION", SecureRandom.uuid)
-    configuration.notify_release_stages = ["production"]
-  end
-end
+# TODO: Replace with sentry
+# if Rails.env.production?
+#   Bugsnag.configure do |configuration|
+#     configuration.auto_notify = Rails.env.production?
+#     configuration.auto_capture_sessions = Rails.env.production?
+#     configuration.api_key = ENV.fetch("BUGSNAG_API_KEY", nil)
+#     configuration.app_version = ENV.fetch("HEROKU_RELEASE_VERSION", SecureRandom.uuid)
+#     configuration.notify_release_stages = ["production"]
+#   end
+# end
