842e34b86da174633146f9ac6f3e9aa50c9b4929
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-03-22 02:29:29 -0700

    Referencing the wrong redis constant

--- lib/source/redis.rb
+++ lib/source/redis.rb
@@ -2,32 +2,32 @@ module Poutineer
   module Redis
     def self.sidekiq_server_connection
       @sidekiq_server_connection ||= ConnectionPool.new(:size => pool_size(:sidekiq_server), :timeout => 5) do
-        Redis.new(:driver => :hiredis, **configuration(:sidekiq))
+        ::Redis.new(:driver => :hiredis, **configuration(:sidekiq))
       end
     end
 
     def self.sidekiq_client_connection
       @sidekiq_client_connection ||= ConnectionPool.new(:size => pool_size(:sidekiq_client), :timeout => 5) do
-        Redis.new(:driver => :hiredis, **configuration(:sidekiq))
+        ::Redis.new(:driver => :hiredis, **configuration(:sidekiq))
       end
     end
 
     def self.objects_connection
       @objects_connection ||= ConnectionPool.new(:size => pool_size(:objects), :timeout => 5) do
-        Redis.new(:driver => :hiredis, **configuration(:objects))
+        ::Redis.new(:driver => :hiredis, **configuration(:objects))
       end
     end
 
     def self.cache_connection
       @cache_connection ||= ConnectionPool.new(:size => pool_size(:cache), :timeout => 5) do
-        Redis.new(:driver => :hiredis, **configuration(:cache))
+        ::Redis.new(:driver => :hiredis, **configuration(:cache))
       end
     end
 
     def self.lock_connection
       @lock_connection ||= Redlock::Client.new([
         ConnectionPool::Wrapper.new(:size => pool_size(:lock), :timeout => 5) do
-          Redis.new(:driver => :hiredis, **configuration(:lock))
+          ::Redis.new(:driver => :hiredis, **configuration(:lock))
         end,
       ])
     end
