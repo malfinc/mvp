8aff1bb8e197165877010d2f8fc1f4a251af6173
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-21 22:27:05 -0700

    Updating with latest patterns

--- @internal/pages/SignUp/SignUpForm/index.js
+++ @internal/pages/SignUp/SignUpForm/index.js
@@ -6,15 +6,26 @@ import get from "@unction/get";
 import view from "@internal/view";
 
 export default view([
-  function SignUpForm ({setEmail, email}) {
+  connect(
+    get("signUpForm"),
+    ({signUpForm: {write}}) => ({write}),
+  ),
+  function SignUpForm ({email, password, write}) {
+    const [form, setForm] = useState({email, password});
+
+    useEffect(function writeEmailState () {
+      write(form);
+    }, [form]);
+
     return <form>
       <Pane>
-        <TextInput onChange={setEmail} value={email} />
+        {form.email}
+        <TextInput type="email" onChange={(event) => setForm({email: event.target.value})} value={email} />
+      </Pane>
+      <Pane>
+        {form.password}
+        <TextInput type="password" onChange={(event) => setForm({password: event.target.value})} value={password} />
       </Pane>
     </form>;
   },
-  connect(
-    get("signUpForm"),
-    pipe([get("signUpForm")]),
-  ),
 ]);
