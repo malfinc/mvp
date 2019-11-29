00a3766dad6d2b5409ce45014038c29f8cdcf036
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-11-28 16:29:14 -0800

    Fixing default password

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :poutineer, Poutineer.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   database: "poutineer_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
