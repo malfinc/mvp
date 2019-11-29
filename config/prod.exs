d6aab143b2c7961033e6f30c7daba4ba1e4ac393
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-11-28 16:29:23 -0800

    Fuck .secret files

--- config/prod.exs
+++ config/prod.exs
@@ -50,6 +50,29 @@ config :logger, level: :info
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-import_config "prod.secret.exs"
+config :poutineer, Poutineer.Repo,
+  # ssl: true,
+  url: System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+config :poutineer, PoutineerWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+# ## Using releases (Elixir v1.9+)
+#
+# If you are doing OTP releases, you need to instruct Phoenix
+# to start each relevant endpoint:
+#
+#     config :poutineer, PoutineerWeb.Endpoint, server: true
+#
+# Then you can assemble a release by calling `mix release`.
+# See `mix help release` for more information.
