a7539ac00762477828ab3e8c8204a41ea27e7f17
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-12-03 01:47:15 -0800

    Expose datetimes

--- lib/poutineer/schema/types.ex
+++ lib/poutineer/schema/types.ex
@@ -1,8 +1,13 @@
 defmodule Poutineer.Schema.Types do
   use Absinthe.Schema.Notation
 
+  import_types Absinthe.Type.Custom
+
   object :account do
     field :id, :id
     field :email, :string
+    field :inserted_at, :naive_datetime
+    field :updated_at, :naive_datetime
+  end
   end
 end
