ef5b857c8a68c7e6d3460ab309ab56a623b141e1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-12-30 02:42:12 -0800

    Use new maps integration

---
+++ lib/poutineer/jobs/fetch_google_places.ex
@@ -0,0 +1,14 @@
+defmodule Poutineer.Jobs.FetchGooglePlaces do
+  use Oban.Worker, queue: :google_places
+
+  @impl Oban.Worker
+  def perform(%{"id" => id}, _job) do
+    record = Poutineer.Repo.get(Poutineer.Models.Establishment, id)
+
+    GoogleMaps.place_details(record.google_place_id)
+      |> case do
+        {:ok, place} -> record |> Ecto.Changeset.change(google_place_data: place) |> Poutineer.Repo.update()
+        {:error, exception, reason} -> {:error, {exception, reason}}
+      end
+  end
+end
