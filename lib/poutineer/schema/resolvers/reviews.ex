8aa280870108598451d4309bc31da9eafd504a3a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-01-04 14:30:57 -0800

    Allow reviews to be created

--- lib/poutineer/schema/resolvers/reviews.ex
+++ lib/poutineer/schema/resolvers/reviews.ex
@@ -6,4 +6,13 @@ defmodule Poutineer.Schema.Resolvers.Reviews do
   def fetch(_parent, %{id: id}, _resolution) when not is_nil(id) do
     {:ok, Poutineer.Repo.get(Review, id)}
   end
+
+  def create(_parent, arguments, %{context: %{current_account: %Poutineer.Models.Account{id: id}}}) when not is_nil(id) do
+    %Poutineer.Models.Review{}
+      |> Poutineer.Models.Review.changeset(Map.merge(arguments, %{author_account_id: id}))
+      |> case do
+        %Ecto.Changeset{valid?: true} = changeset -> Poutineer.Repo.insert(changeset)
+        %Ecto.Changeset{valid?: false} = changeset -> {:error, changeset}
+      end
+  end
 end
